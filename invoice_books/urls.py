3ab16fe7d45004184b063a8b9680e1c972d8099a
Saurabh Goyal <saurabh.goyal@joshtechnologygroup.com>
2017-07-15 09:06:09 +0530

    base set-up of project

---
+++ invoice_books/urls.py
@@ -0,0 +1,12 @@
+"""
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+"""
+
+from django.conf.urls import url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls)
+]
