a4d306bbbcfb7233c04121272ebca7612d02de7b
Saurabh Goyal <saurabh.2561@gmail.com>
2017-07-17 16:26:13 +0530

    add tax in item create/update

--- applications/inventories/forms.py
+++ applications/inventories/forms.py
@@ -6,8 +6,8 @@ from applications.inventories import models as inventories_models
 from applications.taxes import models as taxes_models
 
 
-class ItemProcessFormMixin(object):
-    """ Form Mixin for item create/update page """
+class BaseItemProcessForm(forms.ModelForm):
+    """ Base Form for item create/update page """
 
     tax_1 = forms.ModelChoiceField(
         label='Primary Tax', help_text='Primary tax to be applied on the item', queryset=taxes_models.Tax.objects.all(), required=False
@@ -16,19 +16,20 @@ class ItemProcessFormMixin(object):
         label='Secondary Tax', help_text='Secondary tax to be applied on the item', queryset=taxes_models.Tax.objects.all(), required=False
     )
 
-    # def clean_name(self):
-    #     return self.cleaned_data.get('name').strip().lower().title()
+    class Meta:
+        model = inventories_models.Item
+        fields = ('sku', 'hsn', 'name', 'description', 'price', 'quantity', 'tax_1', 'tax_2', 'tax_notes')
 
-    # def clean(self):
-    #     cleaned_data = super(ItemProcessFormMixin, self).clean()
-    #     tax_1 = cleaned_data.get('tax_1')
-    #     tax_2 = cleaned_data.get('tax_2')
-    #     if tax_1 and tax_2 and tax_1 == tax_2:
-    #         raise forms.ValidationError(
-    #             _('Same tax [%(tax)s] can not be applied twice.'),
-    #             code="invalid_tax", params={'tax': tax_1}
-    #         )
-    #     return cleaned_data
+    def clean(self):
+        cleaned_data = super(BaseItemProcessForm, self).clean()
+        tax_1 = cleaned_data.get('tax_1')
+        tax_2 = cleaned_data.get('tax_2')
+        if tax_1 and tax_2 and tax_1 == tax_2:
+            raise forms.ValidationError(
+                _('Same tax [%(tax)s] can not be applied twice.'),
+                code="invalid_tax", params={'tax': tax_1}
+            )
+        return cleaned_data
 
     def _validate_sku(self, sku, company):
         if inventories_models.Item.all_objects.exclude(
@@ -40,25 +41,25 @@ class ItemProcessFormMixin(object):
             )
         return sku
 
-    # def save(self, *args, **kwargs):
-    #     item = super(ItemProcessFormMixin, self).save(*args, **kwargs)
-    #     tax_1 = self.cleaned_data.get('tax_1')
-    #     tax_2 = self.cleaned_data.get('tax_2')
-    #     if tax_1:
-    #         taxes_models.ItemTax.objects.create(item=item, tax=tax_1)
-    #     if tax_2:
-    #         taxes_models.ItemTax.objects.create(item=item, tax=tax_2)
-    #     return item
+    def save(self, *args, **kwargs):
+        item = super(BaseItemProcessForm, self).save(*args, **kwargs)
+        item.taxes.all().delete()
+        tax_1 = self.cleaned_data.get('tax_1')
+        tax_2 = self.cleaned_data.get('tax_2')
+        if tax_1:
+            taxes_models.ItemTax.all_objects.update_or_create(defaults={'active': True}, item=item, tax=tax_1)
+        if tax_2:
+            taxes_models.ItemTax.all_objects.update_or_create(defaults={'active': True}, item=item, tax=tax_2)
+        return item
 
 
-class ItemCreateForm(ItemProcessFormMixin, forms.ModelForm):
+class ItemCreateForm(BaseItemProcessForm):
     """ Form for item create page """
 
     company = forms.ModelChoiceField(queryset=companies_models.Company.objects.all(), required=False)
 
-    class Meta:
-        model = inventories_models.Item
-        exclude = ("active", "created_at", "updated_at")
+    class Meta(BaseItemProcessForm.Meta):
+        fields = BaseItemProcessForm.Meta.fields + ('company',)
 
     def clean_company(self):
         return self.initial['company']
@@ -67,12 +68,8 @@ class ItemCreateForm(ItemProcessFormMixin, forms.ModelForm):
         return self._validate_sku(self.cleaned_data['sku'], self.initial['company'])
 
 
-class ItemUpdateForm(ItemProcessFormMixin, forms.ModelForm):
+class ItemUpdateForm(BaseItemProcessForm):
     """ Form for item update page """
-
-    class Meta:
-        model = inventories_models.Item
-        exclude = ("company", "active", "created_at", "updated_at")
 
     def clean_sku(self):
         return self._validate_sku(self.cleaned_data['sku'], self.instance.company)
